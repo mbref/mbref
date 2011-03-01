87a7f857cd7029555be722ac08e88fd0e0d416ae
Stephan Linz <linz@li-pro.net>
2011-03-01 17:02:40 +0100

    tpos: linux: remove PROJECT_NAME in Kconfig
    Remove the improper kernel configuration PROJECT_NAME.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- edk-repository/ThirdParty/bsp/linux_v1_00_a/data/linux_v2_1_0.tcl
+++ edk-repository/ThirdParty/bsp/linux_v1_00_a/data/linux_v2_1_0.tcl
@@ -97,11 +97,11 @@ proc generate_linux {os_handle} {
 
 	puts ${cfg_file_kc} "\# PTXdist section key: this is part of the Xilinx BSP for a SoftCPU"
 	puts ${cfg_file_kc} "\#\# SECTION=xlbsp_softcpu\n"
-	puts ${cfg_file_kc} "\# Project name"
-	puts ${cfg_file_kc} "comment \"Project name\"\n"
-	puts ${cfg_file_kc} "config PROJECT_NAME"
-	puts ${cfg_file_kc} "\tstring \"Project name\""
-	puts ${cfg_file_kc} "\tdefault \"[file tail [get_project_folder]]\"\n\n"
+#	puts ${cfg_file_kc} "\# Project name"
+#	puts ${cfg_file_kc} "comment \"Project name\"\n"
+#	puts ${cfg_file_kc} "config PROJECT_NAME"
+#	puts ${cfg_file_kc} "\tstring \"Project name\""
+#	puts ${cfg_file_kc} "\tdefault \"[file tail [get_project_folder]]\"\n\n"
 
 	variable pkg_name
 	put_pkg_cfg ${pkg_name} ${cfg_file_ac} ${os_handle}
