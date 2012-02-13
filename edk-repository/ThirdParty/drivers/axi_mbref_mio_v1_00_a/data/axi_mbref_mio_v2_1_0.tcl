75362b243306c1e9bf6e66dbf446d66d17ccddbc
Stephan Linz <linz@li-pro.net>
2012-02-13 22:42:26 +0100

    tpos: axi_mbref_mio: new IP core
    MicroBlaze References simple memory bank for AXI4.
    This belongs to MicroBlaze References project.
    
    It still need the software reset register to be
    compatible to the plbv46_mbref_mio IP core. There
    is a problem with the Xilinx IP wizzard (createip)
    in Xilinx EDK 13.1 and 13.2.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ edk-repository/ThirdParty/drivers/axi_mbref_mio_v1_00_a/data/axi_mbref_mio_v2_1_0.tcl
@@ -0,0 +1,29 @@
+#
+# (C) Copyright 2012
+# Li-Pro.Net <www.li-pro.net>
+# Stephan Linz <linz@li-pro.net>
+#
+# Filename:     axi_mbref_mio_v2_1_0.tcl
+# Description:  Microprocess Driver Command (tcl)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+#
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "axi_mbref_mio" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_MEM0_BASEADDR" "C_S_AXI_MEM0_HIGHADDR" "C_S_AXI_MEM1_BASEADDR" "C_S_AXI_MEM1_HIGHADDR" "C_S_AXI_MEM2_BASEADDR" "C_S_AXI_MEM2_HIGHADDR" "C_S_AXI_MEM3_BASEADDR" "C_S_AXI_MEM3_HIGHADDR" 
+}
